19186b1a85ce71aeaa274f7f17f2c43ade7e434c
Greg Langford <greg@langford.me>
2017-04-04 22:34:10 +0100

    fixed

--- main.c
+++ main.c
@@ -8,23 +8,22 @@
 
 LOCAL os_timer_t wifi_timeout_timer;
 char pquery[255];
-uint8 bssid[6];
-uint8 ssid[33];
-sint8 rssi;
 uint8 best_bssid[6];
 uint8 best_ssid[33];
 sint8 best_rssi;
+struct station_config stationConf;
 
 void ICACHE_FLASH_ATTR user_esp_platform_dns_found(const	char	*name,	ip_addr_t	*ip,	void *arg)	{
-  os_printf("DNS Callback\n");
   if(ip == NULL) {
     os_printf("nslookup failed\n");
   } else {
-    os_printf("QUERY: %s\tDST: %d.%d.%d.%d",
+    os_printf("QUERY: %s\tDST: %d.%d.%d.%d\n",
     name,
     *((uint8 *)&ip->addr), *((uint8 *)&ip->addr + 1),
     *((uint8 *)&ip->addr + 2), *((uint8 *)&ip->addr + 3));
   }
+
+  gpio16_output_set(1);
 
   user_scan();
 }
@@ -41,6 +40,11 @@ void ICACHE_FLASH_ATTR wifi_handle_event_cb(System_Event_t	*evt) {
 
       struct espconn *pespconn;
       static ip_addr_t ip;
+
+      /* build dns query */
+      os_sprintf(pquery, "%02x-%02x-%02x-%02x-%02x-%02x.q.resolv.cn",
+      best_bssid[0], best_bssid[1], best_bssid[2],
+      best_bssid[3], best_bssid[4], best_bssid[5]);
 
       espconn_gethostbyname(pespconn, pquery, &ip, user_esp_platform_dns_found);
 
@@ -59,14 +63,16 @@ void ICACHE_FLASH_ATTR wifi_timeout() {
 }
 
 void scan_cb(void *arg, STATUS status) {
-  int i = 0;
+  uint8 bssid[6];
+  uint8 ssid[33];
+  uint8 i = 0;
+
+  /* clear persistent variables */
+  os_memset(best_bssid, NULL, os_strlen(best_bssid));
+  os_memset(best_ssid, NULL, os_strlen(best_ssid));
 
   if (status == OK) {
     struct bss_info *bss_link = (struct bss_info *) arg;
-
-    if(bss_link == NULL) {
-      os_printf("Empty Result\n");
-    }
 
     while (bss_link != NULL) {
       os_memset(ssid, 0, 33);
@@ -81,21 +87,31 @@ void scan_cb(void *arg, STATUS status) {
 
       if (bss_link->authmode == AUTH_OPEN) {
 
-        /* set best_ssid on first AP */
+        /* configure best AP to be first AP as default */
         if(i == 0) {
-          os_memset(best_ssid, 0, 33);
           if (os_strlen(bss_link->ssid) <= 32) {
             os_memcpy(best_ssid, bss_link->ssid, os_strlen(bss_link->ssid));
           } else {
             os_memcpy(best_ssid, bss_link->ssid, 32);
           }
 
+          os_memcpy(best_bssid, bss_link->bssid, 6);
+          best_rssi = bss_link->rssi;
+
+          /* set to 1 ensures that this block does not run again */
           i = 1;
         }
 
-        os_printf("%s,"MACSTR",%d\n\n", ssid, MAC2STR(bssid), rssi);
+        /* show scan results */
+        os_printf("%s,"MACSTR",%d\n", ssid, MAC2STR(bssid), bss_link->rssi);
 
-        if(rssi > best_rssi) {
+        /* try and find if link has better rssi */
+        if(bss_link->rssi >= best_rssi) {
+
+          /* must erase previous values otherwise they concatenate */
+          os_memset(best_bssid, NULL, os_strlen(best_bssid));
+          os_memset(best_ssid, NULL, os_strlen(best_ssid));
+
           best_rssi = bss_link->rssi;
 
           if (os_strlen(ssid) <= 32) {
@@ -103,23 +119,38 @@ void scan_cb(void *arg, STATUS status) {
           } else {
             os_memcpy(best_ssid, bss_link->ssid, 32);
           }
+
+          os_memcpy(best_bssid, bss_link->bssid, 6);
         }
       }
       bss_link = bss_link->next.stqe_next;
     }
 
-    os_printf("\n\nbest network\n");
-    os_printf("%s, %d\n\n\n", best_ssid, best_rssi);
+    /* display the best network */
+    os_printf("\nbest network\n");
+    os_printf("%s,"MACSTR",%d\n\n", best_ssid, MAC2STR(best_bssid), best_rssi);
+
+    os_memcpy(&stationConf.bssid, best_bssid, 6);
+    os_memcpy(&stationConf.ssid, best_ssid, 33);
+    stationConf.bssid_set = 1;
+
+    os_timer_disarm(&wifi_timeout_timer);
+    os_timer_setfn(&wifi_timeout_timer, (os_timer_func_t *)wifi_timeout, NULL);
+    os_timer_arm(&wifi_timeout_timer, 15000, 0);
+
+    wifi_station_disconnect();
+    wifi_station_set_config(&stationConf);
+    wifi_station_connect();
 
   } else {
-    os_printf("Scan failed\n");
-    user_scan();
+    os_printf("scanning failed, trying again\n");
+    system_restart();
   }
-
-  user_scan();
 }
 
 void ICACHE_FLASH_ATTR user_scan() {
+  /* turn off got ip led */
+
   if(wifi_get_opmode() == SOFTAP_MODE)
   {
    os_printf("ap mode can't scan !!!\r\n");
